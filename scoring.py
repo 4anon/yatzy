51523249d8e574594c7daa96f35e9709d83145d8
AF2310 <aferat321@gmail.com>
2024-10-28 00:21:52 +0100

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,3 +1,16 @@
+import random
+def diceroll(reroll):
+    rolls=[]
+    if not reroll:
+      for i in range(5):
+       roll=random.randint(1,6)
+       rolls.append(roll)
+    elif reroll:
+       for i in range(len(reroll)):
+          if reroll[i]==9:
+             reroll[i]=random.randint(1,6)
+       rolls=reroll
+    return rolls
 
 def totalscore(dice):
     return sum(dice)
